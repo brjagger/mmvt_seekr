0d5659d4cea570b2584f72bf86b6331d1d9985da
brjagger <bjagger@ucsd.edu>
2019-09-18 14:17:39 -0700

    initial seekr prep code commit and some testing files

---
+++ mmvt_seekr/seekr.py
@@ -0,0 +1,151 @@
+"""
+seekr.py
+control script for generating all input files for SEEKR calculation
+
+Parameters
+		----------
+		calc_type: string default= "off" 
+				type of calculation, options "on" or "off"
+		model: object Required
+			the SEEKR milestoning model
+		bound_dict: dictionary Required
+			dictionary of bound states in the milestoning model
+
+
+
+		Returns
+		-------
+		model : class 
+				contains all required information for milestoning analysis
+"""
+
+
+##Parse SEEKR input file###
+
+import argparse
+import os
+
+inputs = { #contains all DEFAULT parameters for seekr input file
+	###Genaral Parameters###
+	'empty_rootdir':False,
+}
+
+def _parse_seekr_input(inp_filename):
+	new_inputs = {}
+	listopen = False # this gives us the ability to read parameters across several lines
+	ourlist = []
+	for line in open(inp_filename,'r'):
+		splitline = line.strip().split() + ['#']
+
+		if not listopen: splitlist = []
+		for i in range(len(splitline)):
+			if splitline[i][0] == '[':
+				splitline[i] = splitline[i][1:].strip()
+				listopen = True
+				if splitline[i] and splitline[i][-1] == ']':
+					listopen = False
+					break
+			if splitline[i] and splitline[i][-1] == ']':
+				listopen = False
+				splitlist.append(splitline[i][:-1])
+				break
+
+			if splitline[i].startswith('#'): # the remainder of this line is a comment
+				break
+			splitlist.append(splitline[i].strip())
+
+		if not listopen:
+			if len(splitlist) < 2: 
+				continue
+			word1 = splitlist[0].lower()
+			word2 = " ".join(splitlist[1:])
+			if word1[0] == '#': # this line is a comment
+				continue
+			new_inputs[word1] = word2 # populate the input dictionary with this value
+	return new_inputs
+
+def _get_inputs(args,):
+# combine the default input with the user-defined input
+#inputscript = sys.argv[1] # the input file for SEEKR
+	if args['input_filename']:
+		input_filename = args['input_filename']
+		new_inp = _parse_seekr_input(input_filename)
+		print(new_inp)
+		inputs.update(new_inp)
+		print(inputs)
+	else:
+		unittest.main()
+		exit()
+	return inputs
+
+def _get_sys_params(inp):
+	sys_params = {#list of parameters needed to generate filetree
+		'project_name':inp['project_name'],
+		'rootdir':inp['rootdir'],
+		'system_pdb_filename':inp['system_pdb_filename'], #Protein-Ligand complex
+		'system_parm_filename':inp['system_parm_filename'], #for AMBER FF
+		'system_psf_filename':inp['system_psf_filename'],   # for CHARMM FF
+		'lig_pqr_filename':inp['lig_pqr_filename'], #for BD simulations
+		'rec_dry_pqr_filename':inp['rec_dry_pqr_filename'], #for BD simulations
+		'empty_rootdir':_boolean(inp['empty_rootdir']),
+		}
+	return sys_params
+
+def _boolean(arg):
+  if str(arg).lower() in ("false","", "0", "0.0", "[]", "()", "{}"):
+    return False
+  else:
+    return True
+
+def _parse_milestone_inputs(inp):
+	milestones = []
+	for key in in sorted(inp.keys()):
+		if re.match("milestone[0-9]+", key):
+			milestone_dict = {'key': key}
+			for param in inp[key].split(','):
+				split_param = param.strip().split()
+				if len(split_param) < 2: continue
+				milestone_key = split_param[0]
+				milestone_value = ' '.join(split_param[1:]) 
+				milestone_dict[milestone_key] = milestone_value
+			milestones.append(milestone_dict)
+	return milestones
+
+def _generate_milestone_lists(milestones):
+	
+
+def _generate_filetree(inp, sys_params):
+	print("Creating file tree at location: %s" % inp['rootdir'])
+	if sys_params['empty_rootdir'] and os.path.isdir(inp['rootdir']): # then we're in test mode, delete anything that is in the rootdir
+		print("'empty_rootdir' set to True. Deleting all subdirectories/files in rootdir:", inp['rootdir'])
+		shutil.rmtree(inp['rootdir']) # remove all the files within the grid subdirectory
+	if not os.path.isdir(inp['rootdir']): # if the rootdir doesn't exist yet
+		os.mkdir(inp['rootdir']) # then create it
+	return
+
+
+
+def _prepare_equil():
+
+	return
+
+def _prepare_prod():
+
+	return
+
+def prepare_seekr():
+	parser = argparse.ArgumentParser(description="top level SEEKR program used to prepare and generate all input files necessary for a SEEKR calculation")
+	parser.add_argument('input_filename', metavar='INPUT_FILENAME', type = str, help="name of SEEKR input file")
+	args = parser.parse_args() #parse command line arguments
+	args = vars(args) #converts to dictionary
+
+	_get_inputs(args,)
+	sys_params = _get_sys_params(inputs)
+	_generate_filetree(inputs, sys_params)
+	milestones = _parse_milestone_inputs(inputs)
+
+
+
+if __name__ == "__main__": prepare_seekr()
+
+
